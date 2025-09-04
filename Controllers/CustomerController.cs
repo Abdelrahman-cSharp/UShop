3558e60199b629fddf6e3a45f4c83d82b75de439
PC <PC@BASMALAELGOHARY>
2025-09-04 16:22:11 +0300

    customer controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UShop.Models;
+using System.Linq;
+using UShop.Data;
+
+namespace UShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly UShopDBContext _context;
+
+        public CustomerController(UShopDBContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var customers = _context.Customers
+                .Include(c => c.Orders) 
+                .ToList();
+            return View(customers);
+        }
+
+        
+        public IActionResult Details(int id)
+        {
+            var customer = _context.Customers
+                .Include(c => c.Orders)
+                .ThenInclude(o => o.Items)
+                .FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+      
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Customers.Any(e => e.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+       
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers
+                .FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+       
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
