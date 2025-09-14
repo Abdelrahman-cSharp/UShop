7940ddb67de78d894819d768f8f21705130d7db3
Ahmed <Ahmed@DESKTOP-O69KSQ2>
2025-09-14 10:59:55 +0300

    Migriation

---
+++ Migrations/20250914075841_delete_CreadeCard_From_Customer_and_Seller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class delete_CreadeCard_From_Customer_and_Seller : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
