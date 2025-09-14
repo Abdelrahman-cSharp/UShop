e66fe46b8eeb127534b99b50fdb02fe8f69530cb
Abdelrahman-cSharp <abdelrahmanr182003@gmail.com>
2025-09-14 18:11:29 +0300

    fix: layout

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,9 +9,11 @@ namespace UShop.Models
 		public int Id { get; set; }
 		[Required, MaxLength(100)]
 		public string FullName { get; set; } = string.Empty;
+
 		[Required, MaxLength(100)]
 		[EmailAddress]
 		public string Email { get; set; } = string.Empty;
+
 		[MaxLength(15)]
 		public string? PhoneNumber { get; set; }
 
